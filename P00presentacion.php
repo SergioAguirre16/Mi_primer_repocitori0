9cefc5cb02f6f2312cfefa51072f29f307bb2c53
SergioAguirre16 <sergioalejandroaguirre1@gmail.com>
2024-10-21 09:30:28 -0600

    Add files via upload

---
+++ P00presentacion.php
@@ -0,0 +1,17 @@
+<?php
+/*CBTIS 89
+P00precentacion
+Programa que da mis datos personales
+Sergio Alejandro  Aguirre Ortiz
+3°A Programación Matutino
+6182166275
+Vivo en la tierra*/
+
+
+echo "Mi nombre es Sergio Alejandro Aguirre Ortiz";
+echo "<br>","<br>";
+echo "Estudio en el CBTIS#89";
+echo "<br>","<br>";
+echo "3°A Programación turno Matutino";
+echo "<br>","<br>";
+?>
